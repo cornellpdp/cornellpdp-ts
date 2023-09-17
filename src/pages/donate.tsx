da50bd510cd3303785178e4fe9fb367e23f46a19
Max Wang <maxwang@dhcp-vl2042-23100.redrover.cornell.edu>
2023-09-17 16:06:06 -0400

    made donate page prettier

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -1,21 +1,45 @@
+import React, { CSSProperties } from 'react';
 
 const Donate: React.FC = () => {
-  const handleButtonClick = () => {
+  const openExternalLink = () => {
     // Replace 'https://example.com' with the external link you want to open.
     window.open('https://www.paypal.com/paypalme/CornellPiDeltaPsi?country.x=US&locale.x=en_US', '_blank');
   };
 
+  const containerStyle: CSSProperties = {
+    textAlign: 'center',
+    backgroundColor: '#f0f0f0',
+    padding: '20px',
+    borderRadius: '5px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+    maxWidth: '400px',
+    margin: '0 auto',
+    marginTop: '200px', // Add margin-top directly as an inline style
+  };
+
+  const buttonStyle: CSSProperties = {
+    backgroundColor: '#800000',
+    color: '#ffffff',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    marginTop: '20px',
+  };
+
+
   return (
-    <div className="external-link-container">
-      <div style={{ position: "relative" }}>
-        <h1>hi </h1>
-        <p>Pi Delta Psi loves its alumni but sadly cannot afford a fullstack engineer.
-          Please consider donating so that this page can look prettier.
-        </p>
-        <button onClick={handleButtonClick}>Open External Link</button>
-      </div>
-      <br />
-      donate pls
+
+    <div style={containerStyle}>
+      <h1>Pi Delta Psi loves its alumni!
+      </h1>
+      <p>The brothers are working towards strenghtening alumni and family connections
+        towards the goal of a more united brotherhood.
+        Please consider donating so that this page can look prettier.</p>
+      <button style={buttonStyle} onClick={openExternalLink}>
+        Donate
+      </button>
     </div>
   );
 };
